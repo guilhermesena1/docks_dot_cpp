ab045c360c551d34ba549876d86e7db28de95365
guilhermesena1 <desenabr@usc.edu>
2021-09-19 22:57:34 -0700

    working version of binary docks

--- docks.cpp
+++ docks.cpp
@@ -4,7 +4,10 @@
 #include <string>
 #include <fstream>
 #include <algorithm>
+#include <omp.h>
+#include <bitset>
 
+using std::bitset;
 using std::runtime_error;
 using std::ifstream;
 using std::string;
@@ -14,25 +17,27 @@ using std::cout;
 using std::endl;
 using std::max;
 
+typedef uint32_t node_t;
+
 struct DeBruijnGraph {
   explicit DeBruijnGraph(const size_t _k, const size_t _w);
-  double estimate_ram();
+  double estimate_ram() const;
   void exclude_decycling(const string &infile);
-  size_t count_paths_exclude_top();
-  void print_excluded_nodes();
-  void get_nodes_from(const size_t mer, size_t &from_1, size_t &from_2);
-  void get_nodes_to(const size_t mer, size_t &to_1, size_t &to_2);
-  void print_binary(size_t mer);
+  void get_nodes_from(const node_t mer, node_t &from_1, node_t &from_2);
+  void get_nodes_to(const node_t mer, node_t &to_1, node_t &to_2);
+  void print_binary(node_t mer) const;
 
+  bool count_paths_exclude_top();
 
-  size_t sz;
-  size_t hash_mask;
-  size_t top_node;
+  node_t sz; // the number of nodes in the graph
+  size_t hash_mask; // the mask of all 1s in kmers
 
-  vector<bool> V;
-  vector< vector<size_t> > D;
-  vector< vector<size_t> > F;
-  vector< vector<size_t> > T;
+  vector<bool> V; // whether nodes are still in the graph
+  vector<size_t> D;
+  vector<size_t> F;
+  vector<size_t> T;
+
+  vector<node_t> from_1, from_2, to_1, to_2;
   static size_t k;
   static size_t w;
 };
@@ -46,71 +51,99 @@ DeBruijnGraph::DeBruijnGraph(const size_t _k, const size_t _w) {
   sz = (1 << k);
   hash_mask = (1ull << k) - 1;
 
-  cerr << "[required RAM: " <<  estimate_ram() << "]\n";
+  cerr << "[required RAM: " << estimate_ram() << " MB]\n";
   V = vector<bool>(sz, true);
-  D = vector<vector<size_t>>(sz, vector<size_t>(w+1, 0));
-  F = vector<vector<size_t>>(sz, vector<size_t>(w+1, 0));
-  T = vector<vector<size_t>>(sz, vector<size_t>(w+1, 0));
+  
+  const size_t dp_size = sz*(w+1);
+  D = vector<size_t>(dp_size, 0);
+  F = vector<size_t>(dp_size, 0);
+  T = vector<size_t>(sz, 0);
+
+  cerr << "[building edges]\n";
+  from_1 = vector<node_t>(sz);
+  from_2 = vector<node_t>(sz);
+  to_1 = vector<node_t>(sz);
+  to_2 = vector<node_t>(sz);
+  for (node_t i = 0; i < sz; ++i) {
+    get_nodes_from(i, from_1[i], from_2[i]);
+    get_nodes_to(i, to_1[i], to_2[i]);
+  }
 };
 
 double
-DeBruijnGraph::estimate_ram() {
+DeBruijnGraph::estimate_ram() const {
   const size_t num_bytes = 
-    sizeof(bool)*sz + //V
-    sizeof(size_t)*(3*sz*(w+1));
+    sizeof(bool)*sz +
+    sizeof(size_t)*(3*sz*(w+1)) +
+    sizeof(node_t)*4;
 
   return static_cast<double>(num_bytes)/(1024.0*1024.0);
 }
 
 void
-DeBruijnGraph::get_nodes_from(const size_t mer, size_t &from_1, size_t &from_2) {
-  from_1 = (mer >> 1) & hash_mask;
-  from_2 = ((mer >> 1) | (1ull << DeBruijnGraph::k)) & hash_mask;
+DeBruijnGraph::get_nodes_from(const node_t mer, node_t &f1, node_t &f2) {
+  f1 = (mer >> 1) & hash_mask;
+  f2 = ((mer >> 1) | (1ull << (DeBruijnGraph::k - 1))) & hash_mask;
 }
 
 void
-DeBruijnGraph::get_nodes_to(const size_t mer, size_t &to_1, size_t &to_2) {
-  to_1 = (mer << 1) & hash_mask;
-  to_2 = ((mer << 1) | (1)) & hash_mask;
+DeBruijnGraph::get_nodes_to(const node_t mer, node_t &t1, node_t &t2) {
+  t1 = (mer << 1) & hash_mask;
+  t2 = ((mer << 1) | (1)) & hash_mask;
 }
 
-size_t
+bool
 DeBruijnGraph::count_paths_exclude_top() {
-  // get edges from binary hash
-  size_t from_1, from_2, to_1, to_2;
+  const size_t lim = w + 1;
+  const size_t start = 0;
 
-  size_t ans = 0;
-  size_t top_node = 0;
-  for (size_t j = 1; j <= w; ++j) {
-    for (size_t i = 0; i < sz; ++i) {
-      if (j == 1) {
-        D[i][1] = F[i][1] = T[i][1] = V[i];
-      }
-      else if (V[i]) {
-        get_nodes_from(i, from_1, from_2);
-        get_nodes_from(i, to_1, to_2);
-        D[i][j] = V[from_1]*D[from_1][j-1] + V[from_2]*D[from_2][j-1];
-        F[i][j] = V[to_1]*F[to_1][j-1] + V[to_2]*F[to_2][j-1];
-        T[i][j] = D[i][j]*F[i][j];
+#pragma omp parallel for
+  for (node_t i = 0; i < sz; ++i)
+    D[i*lim + start] = F[i*lim + start] = V[i];
+
+  for (size_t j = start + 1; j < lim; ++j) {
+#pragma omp parallel for
+    for (node_t i = 0; i < sz; ++i) {
+      if (V[i]) {
+        // D: paths starting at i with length j
+        D[i*lim + j] = V[to_1[i]]*D[to_1[i]*lim + j - 1] +
+                       V[to_2[i]]*D[to_2[i]*lim + j - 1];
+
+        // F: paths ending at i with length j
+        F[i*lim + j] = V[from_1[i]]*F[from_1[i]*lim + j - 1] +
+                       V[from_2[i]]*F[from_2[i]*lim + j - 1];
+
+
       }
     }
   }
 
-  // find node with most paths
-  for (size_t i = 0; i < sz; ++i)
-    ans = (V[i]) ? (max(ans, T[i][w])) : ans;
+  // count paths through each node
+#pragma omp parallel for
+  for (node_t i = 0; i < sz; ++i) {
+    T[i] = 0;
+    if (V[i]) {
+      for (size_t j = 0; j < lim; ++j) {
+        T[i] += D[i*lim + j]*F[i*lim + lim - j - 1];
+      }
+    }
+  }
 
-  // find the top node
-  for (size_t i = 0; i < sz; ++i)
-    top_node = ((V[i]) && (T[i][w] == ans)) ? (i) : (top_node);
+  // get which node to report
+  const node_t the_best = max_element(begin(T), end(T)) - begin(T);
 
-  // exclude node with most paths
-  print_binary(top_node);
-  V[top_node] = false;
-  return ans;
+  // no more paths of length w
+  if (!V[the_best] || !T[the_best]) return false;
+
+  // report the k-mer with most paths
+  print_binary(the_best);
+
+  // remove it from the graph
+  V[the_best] = false;
+  return true;
 }
 
-static size_t
+static node_t
 to_binary(const string &s) {
   size_t ans = 0ull;
   const size_t lim = s.size();
@@ -130,23 +163,13 @@ DeBruijnGraph::exclude_decycling(const string &infile) {
   string mer;
   while (in >> mer)
     V[to_binary(mer)] = false;
-
 }
 
 void
-DeBruijnGraph::print_binary(size_t mer) {
-  for (size_t i = 0; i < k; ++i) {
-    cout.put((mer & 1) ? '1' : '0');
-    mer >>= 1;
-  }
+DeBruijnGraph::print_binary(const node_t mer) const {
+  for (size_t i = 0; i < k; ++i)
+    cout.put((mer & (1ull << (k - i - 1))) ? '1' : '0');
   cout.put('\n');
-}
-
-void
-DeBruijnGraph::print_excluded_nodes() {
-  for (size_t i = 0; i < sz; ++i)
-    if (!V[i])
-      print_binary(i);
 }
 
 void
@@ -161,24 +184,35 @@ run_docks(const size_t key_weight, const size_t window_size,
   cerr << "[main loop]\n";
   while (G.count_paths_exclude_top() > 0);
 
-  //cerr << "[printing nodes to cout]\n";
-  //G.print_excluded_nodes();
-
   cerr << "[done]\n";
 }
 
 int main(int argc, const char **argv) {
-  if (argc != 4) {
-    cerr << "Usage: docks <k> <w> <decycling-k.txt>" << endl;
+  try {
+    if (argc != 4 && argc != 5) {
+      cerr << "Usage: docks <k> <w> <decycling-k.txt> [num-threads]" << endl;
+      return EXIT_FAILURE;
+    }
+
+    const size_t k = atoi(argv[1]);
+    const size_t w = atoi(argv[2]);
+    const string decycling_file = string(argv[3]);
+    if (argc == 5)
+      omp_set_num_threads(atoi(argv[4]));
+
+    cerr << "[running docks with k = " << k << " and w = " << w << "]\n";
+    cerr << "[using " << omp_get_num_threads() << " threads]\n";
+
+    run_docks(k, w, decycling_file);
+  }
+  catch (const runtime_error &e) {
+    cerr << e.what() << endl;
     return EXIT_FAILURE;
   }
 
-  const size_t k = atoi(argv[1]);
-  const size_t w = atoi(argv[2]);
-  const string decycling_file = string(argv[3]);
-
-  cerr << "[running docks with k = " << k << " and w = " << w << "]\n";
-
-  run_docks(k, w, decycling_file);
+  catch (std::bad_alloc &a) {
+    cerr << "ERROR: could not allocate memory" << endl;
+    return EXIT_FAILURE;
+  }
   return EXIT_SUCCESS;
 }
